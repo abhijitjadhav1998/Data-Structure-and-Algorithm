cb9f9d42d3de1b3e67f0e3b27ef1b8a397acd865
Abhijit Jadhav <abhijit.jadhav1998@gmail.com>
2022-03-21 14:22:08 +0530

    added Selection Sort

--- Sorting/src/SelectionSort.java
+++ Sorting/src/SelectionSort.java
@@ -1,9 +1,21 @@
+import java.util.Arrays;
 
 public class SelectionSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int arr[] = {1,56,49,85,37,2,64,98,56};
+		int unsorted_index = arr.length - 1;
+		for(int i = unsorted_index ; i >= 0;i--) {
+			int largest = 0;
+			for(int j=1;j<=i;j++) {
+				if(arr[largest] < arr[j])
+					largest = j;
+			}
+			int temp = arr[i];
+			arr[i] = arr[largest];
+			arr[largest] = temp;
+		}
+		System.out.println(Arrays.toString(arr));
 	}
 
 }
