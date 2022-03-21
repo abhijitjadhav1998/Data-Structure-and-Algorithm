758e6bd2e07c51a426a0b5ed9814e4be413ae54b
Abhijit Jadhav <abhijit.jadhav1998@gmail.com>
2022-03-21 13:04:37 +0530

    added Bubble Sort

--- Sorting/src/BubbleSort.java
+++ Sorting/src/BubbleSort.java
@@ -1,8 +1,36 @@
+import java.util.Arrays;
+/*
+ * Optimized Bubble Sort
+ * 
+ * Best Case : n
+ * Average Case : n^2
+ * Worst Case : n^2
+ * 
+ */
 
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int arr[] = {1,65,49,35,86,79,62,46,35,65,46};
+		int unsorted_index = arr.length -1;
+		/*
+		 * Does not look for all locations , iterate over n - 1 indexes in every iteration.
+		 */
+		for(int i = unsorted_index; i>= 0; i--) {
+			for(int j = 0;j<i;j++) {
+				if(arr[i] < arr[j]) {
+					/*
+					 * Swap
+					 */
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		
+		System.out.println(Arrays.toString(arr));
 
 	}
 
