3b4956db86a3b59ed571fa742d5eb1cc545b4597
Abhijit Jadhav <abhijit.jadhav1998@gmail.com>
2022-04-08 13:30:43 +0530

    added trees

---
+++ Trees/src/TreeMain.java
@@ -0,0 +1,100 @@
+class TreeNode {
+	private int data;
+	private TreeNode leftChild;
+	private TreeNode rightChild;
+
+	public TreeNode(int value) {
+		this.data = value;
+	}
+
+	public void insert(int value) {
+		if (value == data) {
+			return;
+		}
+		if (value < data) {
+			if (leftChild == null) {
+				leftChild = new TreeNode(value);
+			} else {
+				leftChild.insert(value);
+			}
+		} else {
+			if (rightChild == null) {
+				rightChild = new TreeNode(value);
+			} else {
+				rightChild.insert(value);
+			}
+		}
+	}
+
+	public void traverseInOrder() {
+		if (leftChild != null) {
+			leftChild.traverseInOrder();
+		}
+		System.out.print(data + ", ");
+		if (rightChild != null) {
+			rightChild.traverseInOrder();
+		}
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public TreeNode getLeftChild() {
+		return leftChild;
+	}
+
+	public void setLeftChild(TreeNode leftChild) {
+		this.leftChild = leftChild;
+	}
+
+	public TreeNode getRightChild() {
+		return rightChild;
+	}
+
+	public void setRightChild(TreeNode rightChild) {
+		this.rightChild = rightChild;
+	}
+
+}
+
+class Tree {
+	private TreeNode root;
+
+	public void insert(int value) {
+		if (root == null) {
+			root = new TreeNode(value);
+		} else {
+			root.insert(value);
+		}
+	}
+
+	public void traverseInOrder() {
+		if (root != null) {
+			root.traverseInOrder();
+		}
+	}
+
+}
+
+public class TreeMain {
+
+	public static void main(String[] args) {
+		Tree tree = new Tree();
+		tree.insert(25);
+		tree.insert(20);
+		tree.insert(27);
+		tree.insert(15);
+		tree.insert(22);
+		tree.insert(26);
+		tree.insert(30);
+		tree.insert(29);
+		tree.insert(32);
+		tree.traverseInOrder();
+	}
+
+}
