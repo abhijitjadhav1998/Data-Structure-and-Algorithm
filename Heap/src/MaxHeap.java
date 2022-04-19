acd49710be2c9f4c008b85b1cb94254e4ff2a6c0
Abhijit Jadhav <abhijit.jadhav1998@gmail.com>
2022-04-19 17:40:57 +0530

    added max heap

---
+++ Heap/src/MaxHeap.java
@@ -0,0 +1,20 @@
+class Heap{
+	private int[] heap;
+	private int size;
+	
+	public Heap(int capacity) {
+		super();
+		this.heap = new int[capacity];
+	}
+	
+	
+	
+}
+public class MaxHeap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
